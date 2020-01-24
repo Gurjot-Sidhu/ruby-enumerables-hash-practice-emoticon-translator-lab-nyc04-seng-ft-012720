3aae24c595ea484e9b1e48a154b94cefe37aefff
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-24 05:02:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,18 @@
 # require modules here
 
-def load_library
-  # code goes here
+def load_library(path)
+  library = YAML.load_file(path)
+  new_hash = {}
+  get_meaning = {}
+  get_emoticon = {}
+  library.each do |key, value|
+    get_meaning[(value[1])] = key
+    get_emoticon[(value[0])] = value[1]
+  end
+  new_hash["get_meaning"] = get_meaning
+  new_hash["get_emoticon"] = get_emoticon
+  new_hash
+  
 end
 
 def get_japanese_emoticon
